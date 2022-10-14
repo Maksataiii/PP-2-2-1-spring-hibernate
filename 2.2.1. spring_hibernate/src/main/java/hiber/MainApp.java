da04389caf2a8428eab087741eefeae99bdf5e0d
Maksataiii <erkbvna@gmail.com>
2022-10-14 15:17:56 +0600

    added new classes for Car

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,7 +1,9 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
+import hiber.service.CarService;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -13,12 +15,20 @@ public class MainApp {
       AnnotationConfigApplicationContext context = 
             new AnnotationConfigApplicationContext(AppConfig.class);
 
+      CarService carService = context.getBean(CarService.class);
+      carService.add(new Car("ferrari",123));
+      carService.add(new Car("bmw",1234));
+      carService.add(new Car("audi",12345));
+      carService.add(new Car("Acura",785));
+
+      List<Car> cars = carService.listCars();
+
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru",cars.get(0)));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru",cars.get(1)));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru",cars.get(2)));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru",cars.get(3)));
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,9 +36,9 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         System.out.println("Car = "+user.getCar());
          System.out.println();
       }
-
       context.close();
    }
 }
